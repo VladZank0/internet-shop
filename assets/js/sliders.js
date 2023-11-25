d630a49a21f6a71cca69d2e99b21490578db1bc5
Vlad Zanko <vlad.zanko.2019@mail.ru>
2023-11-25 22:12:40 +0300

    made compare and 404 pages

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -19,7 +19,8 @@ const hero_slider = new Swiper ('.hero__slider', {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    }
+    },
+    
     
 })
 const product_slider = new Swiper ('.product-slider', {
@@ -44,7 +45,7 @@ const product_slider = new Swiper ('.product-slider', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     }
-      
+   
     
 })
 const blog_slider = new Swiper ('.blog-slider', {
@@ -61,7 +62,6 @@ const blog_slider = new Swiper ('.blog-slider', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     }
-    
 })
 const selections_slider = new Swiper ('.selections__item_slider', {
 
@@ -80,14 +80,94 @@ const selections_slider = new Swiper ('.selections__item_slider', {
     }
 })
 const news_slider = new Swiper ('.news__slider', {
-    spaceBetween: 29,
-    watchOverflow: true,
-    slidesPerView: "auto",
-    centeredSlidesBounds:true,
-    observer: true,
-    observeParents: true,
-    loop:false,
-    slidesOffsetAfter:false,
-    dynamicBullets: true
+  spaceBetween: 29,
+  watchOverflow: true,
+  slidesPerView: "auto",
+  centeredSlidesBounds:true,
+  observer: true,
+  observeParents: true,
+  loop:false,
+  slidesOffsetAfter:false,
+  dynamicBullets: true
 })
 
+
+const compareSlider = new Swiper('.comparison-page__main-slider', {
+  spaceBetween: 55,
+  slidesPerView: "auto",
+  breakpoints: {
+    0: {
+      spaceBetween: 16
+    },
+    577: {
+      spaceBetween: 55
+    }
+  },
+
+  navigation: {
+    nextEl: '.comparison-page__main-slider-button-next',
+    prevEl: '.comparison-page__main-slider-button-prev',
+  },
+
+});
+
+const compareSlider_fixed = new Swiper('.comparison-page__main-fixed-slider', {
+  spaceBetween: 55,
+  slidesPerView: "auto",
+  breakpoints: {
+    0: {
+      spaceBetween: 16
+    },
+    577: {
+      spaceBetween: 55
+    }
+  },
+  navigation: {
+    nextEl: '.comparison-page__main-slider-button-next',
+    prevEl: '.comparison-page__main-slider-button-prev',
+  },
+
+});
+// Инициализация дочерних слайдеров
+const dataSliders = document.querySelectorAll('.comparison-page__row-slider');
+
+const childSliders = [compareSlider_fixed];
+
+dataSliders.forEach(function (dataSlider) {
+  const childSlider = new Swiper(dataSlider, {
+    spaceBetween: 55,
+    slidesPerView: "auto",
+    observer: true,
+    observeParents: true,
+    slideToClickedSlide: true,
+    watchSlidesProgress: true,
+    loop: false,
+    breakpoints: {
+      // when window width is >= 320px
+      0: {
+        spaceBetween: 16
+      },
+      577: {
+        spaceBetween: 55
+      }
+    },
+  });
+
+
+  childSliders.push(childSlider);
+});
+
+// Установка массива дочерних слайдеров в качестве контроллера для основного слайдера
+compareSlider.controller.control = childSliders;
+compareSlider_fixed.controller.control = compareSlider;
+
+window.addEventListener('scroll', function () {
+  const triggerBlockTop = document.querySelector('.comparison-page__main-slider').getBoundingClientRect().top;
+  const isVisible = window.scrollY - 450 > triggerBlockTop;
+
+  if (isVisible) {
+      document.querySelector('.comparison-page__main-fixed-slider').classList.add('comparison-page__main-fixed-slider_shown');
+  } else {
+      document.querySelector('.comparison-page__main-fixed-slider').classList.remove('comparison-page__main-fixed-slider_shown');
+  }
+})
