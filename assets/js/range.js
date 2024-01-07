bfbdccc51f06a96a1be9e5436d5a398c80fc4f46
Vlad Zanko <vlad.zanko.2019@mail.ru>
2024-01-07 23:30:43 +0300

    commit

--- assets/js/range.js
+++ assets/js/range.js
@@ -21,21 +21,21 @@
           var activePips = [null, null];
           
           credit_range.noUiSlider.on('update', function (values, handle) {
-              // Remove the active class from the current pip
-              if (activePips[handle]) {
-                  activePips[handle].classList.remove('active-pip');
-              }
-          
-              // Match the formatting for the pip
-              var dataValue = Math.round(values[handle]);
-          
-              // Find the pip matching the value
-              activePips[handle] = credit_range.querySelector('.noUi-value[data-value="' + dataValue + '"]');
-          
-              // Add the active class
-              if (activePips[handle]) {
-                  activePips[handle].classList.add('active-pip');
-              }
+            // Remove the active class from the current pip
+            if (activePips[handle]) {
+                activePips[handle].classList.remove('active-pip');
+            }
+        
+            // Match the formatting for the pip
+            var dataValue = Math.round(values[handle]);
+        
+            // Find the pip matching the value
+            activePips[handle] = credit_range.querySelector('.noUi-value[data-value="' + dataValue + '"]');
+        
+            // Add the active class
+            if (activePips[handle]) {
+                activePips[handle].classList.add('active-pip');
+            }
           });
     }
   
